af3bdc4019afe93529c4e41aea787dc657e5212a
kaifee17 <kaifee17@example.com>
2025-06-22 21:10:14 +0530

    Update vite config and deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MYproject/', // 👈 Required for GitHub Pages
 })
