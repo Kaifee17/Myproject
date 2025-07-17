3bbcee2e6adcbc1adf30cc1af673eda0cd9f2a84
kaifee17 <kaifee17@example.com>
2025-07-17 16:05:10 +0530

    idk

--- Client/src/pages/Contact.jsx
+++ Client/src/pages/Contact.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { postData } from '../utils/api';
 import AppContext from '../context/AppContext';
-
 const Contact = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [formFields, setFormFields] = useState({
